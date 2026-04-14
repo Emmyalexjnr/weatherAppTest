26bfd24392ce0f00708be74b9f6baf28f88f68fa
emmyalexjnr <emmyalexjnr@gmail.com>
2026-04-14 12:30:02 +0100

    feat: implement navigation to weather screen with current location in WeatherCurrent component

--- src/components/WeatherCurrent.tsx
+++ src/components/WeatherCurrent.tsx
@@ -1,10 +1,19 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import LocationService from '../services/LocationService';
+import Button from './Button';
+import { RootStackParamList } from '../screens';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 export default function WeatherCurrent() {
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+  const handleFetchWeather = useCallback(async () => {
+    const position = await LocationService.getCurrentLocation();
+    navigation.navigate('Weather', position);
+  }, [navigation])
   return (
-    <View testID="weather-current">
-      <Text>WeatherCurrent</Text>
-    </View>
+    <Button testID='weather-current' label='' onPress={handleFetchWeather} />
   )
 }
--- src/components/__tests__/WeatherCurrent.test.tsx
+++ src/components/__tests__/WeatherCurrent.test.tsx
@@ -1,5 +1,13 @@
-import { render } from "@testing-library/react-native"
+import { fireEvent, render, waitFor } from "@testing-library/react-native"
 import WeatherCurrent from "../WeatherCurrent"
+import { useNavigation } from "@react-navigation/native"
+
+jest.mock('@react-navigation/native', () => {
+    return {
+        ...jest.requireActual<object>('@react-navigation/native'),
+        useNavigation: jest.fn()
+    }
+})
 
 describe('WeatherCurrent', () => {
     test('Should render correctly', () => {
@@ -7,7 +15,20 @@ describe('WeatherCurrent', () => {
         wrapper.getByTestId('weather-current')
     })
 
-    test("Should navigate to weather screen with location", () => {
-        throw new Error("Not implemented")
+    test("Should navigate to weather screen with location", async () => {
+        // throw new Error("Not implemented")
+        const mockNavigate = jest.fn();
+        (useNavigation as jest.Mock).mockReturnValueOnce({ navigate: mockNavigate })
+        const wrapper = render(<WeatherCurrent />)
+        const button = wrapper.getByTestId('weather-current');
+        fireEvent.press(button)
+
+        await waitFor(() => {
+            expect(mockNavigate).toHaveBeenCalledWith('Weather', {
+                latitude: 0,
+                longitude: 0
+            })
+        })
+
     })
 })
